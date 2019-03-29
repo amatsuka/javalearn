efc9698b63092dc8fe3fdafd6d47fa9cb8c61d96
amatsuka <run100500@gmail.com>
2019-03-29 18:45:17 +0300

    Доработка домашки по паттернам

--- src/main/java/com/amatsuka/excercies/patterns/ex2/UserToAnimalsAdapter.java
+++ src/main/java/com/amatsuka/excercies/patterns/ex2/UserToAnimalsAdapter.java
@@ -3,9 +3,9 @@ package com.amatsuka.excercies.patterns.ex2;
 import com.amatsuka.excercies.patterns.ex1.Animal;
 import com.amatsuka.excercies.patterns.ex1.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class UserToAnimalsAdapter {
     private final User user;
@@ -15,12 +15,25 @@ public class UserToAnimalsAdapter {
     }
 
     public List<Animal> animals() {
-        List<Animal> result = user.getRelatives().stream().map(User::getPet).filter(Objects::nonNull).collect(Collectors.toList());
+        return getAnimals(user);
+    }
+
+    private List<Animal> getAnimals(User user) {
+        List<Animal> animals = new ArrayList<>();
 
         if (user.getPet() != null) {
-            result.add(user.getPet());
+            animals.add(user.getPet());
         }
 
-        return result;
+        if (user.getRelatives() != null) {
+            user.getRelatives().stream()
+                    .filter(Objects::nonNull)
+                    .flatMap(relative -> getAnimals(relative).stream())
+                    .filter(Objects::nonNull)
+                    .forEach(animals::add);
+
+        }
+
+        return animals;
     }
 }
